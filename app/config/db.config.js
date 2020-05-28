4456118b8217b8e3d51e0e1173348c8603e11462
rahul.kumar7 <rahul.kumar7@timesinternet.in>
2020-05-28 10:47:40 +0530

    db credentials updated

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "us-cdbr-iron-east-02.cleardb.net",
-  USER: "b7e24378878xxx",
-  PASSWORD: "0200exxx",
-  DB: "heroku_7643ec736354xxx"
+  HOST: "sql12.freemysqlhosting.net",
+  USER: "sql12343824",
+  PASSWORD: "dc3FE2svqG",
+  DB: "sql12343824"
 };
